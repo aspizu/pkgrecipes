9bb95fef7416d7191ae504335d9b3b2a1d2b757b
Priyanshu Dangare <aspizu@protonmail.com>
2025-10-20 10:28:15 +0000

    add pkg gcc

---
+++ recipes/gcc/build.sh
@@ -0,0 +1,21 @@
+case $(uname -m) in
+  x86_64)
+    sed -e '/m64=/s/lib64/lib/' \
+        -i.orig gcc/config/i386/t-linux64
+  ;;
+esac
+mkdir build
+cd build
+../configure --prefix=/usr            \
+             LD=ld                    \
+             --enable-languages=c,c++ \
+             --enable-default-pie     \
+             --enable-default-ssp     \
+             --enable-host-pie        \
+             --disable-multilib       \
+             --disable-bootstrap      \
+             --disable-fixincludes    \
+             --with-system-zlib
+make
+make install
+ln -sr /usr/bin/cpp $DESTDIR/usr/lib
