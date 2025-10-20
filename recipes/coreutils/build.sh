6ed757755953ee990fa1977f7f4e1338af72ab18
Priyanshu Dangare <aspizu@protonmail.com>
2025-10-20 17:06:16 +0000

    add pkg gnu coreutils

---
+++ recipes/coreutils/build.sh
@@ -0,0 +1,6 @@
+FORCE_UNSAFE_CONFIGURE=1 ./configure --prefix=/usr --enable-no-install-program=kill,uptime
+make
+make DESTDIR=$DESTDIR install
+mv $DESTDIR/usr/bin/chroot $DESTDIR/usr/sbin
+mv $DESTDIR/usr/share/man/man1/chroot.1 $DESTDIR/usr/share/man/man8/chroot.8
+sed -i 's/"1"/"8"/' $DESTDIR/usr/share/man/man8/chroot.8
