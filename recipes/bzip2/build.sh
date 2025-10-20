28d5d99da7e08fbd4633d4a70d0e0b7affa3bf0a
Priyanshu Dangare <aspizu@protonmail.com>
2025-10-20 02:52:11 +0000

    add bzip2 package

---
+++ recipes/bzip2/build.sh
@@ -0,0 +1,14 @@
+sed -i 's@\(ln -s -f \)$(PREFIX)/bin/@\1@' Makefile
+sed -i "s@(PREFIX)/man@(PREFIX)/share/man@g" Makefile
+make -f Makefile-libbz2_so
+make clean
+make
+make PREFIX=/usr install
+mkdir -p $DESTDIR/usr/{bin,lib}
+cp -av libbz2.so.* $DESTDIR/usr/lib
+ln -sv libbz2.so.1.0.8 $DESTDIR/usr/lib/libbz2.so
+cp -v bzip2-shared $DESTDIR/usr/bin/bzip2
+for i in $DESTDIR/usr/bin/{bzcat,bunzip2}; do
+  ln -sfv bzip2 $i
+done
+rm -fv $DESTDIR/usr/lib/libbz2.a
