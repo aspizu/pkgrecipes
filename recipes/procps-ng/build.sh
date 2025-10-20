ac0aa4f8d8bda01c17bd396edf8327b3f1153580
Priyanshu Dangare <aspizu@protonmail.com>
2025-10-20 19:46:09 +0000

    add pkg procps-ng

---
+++ recipes/procps-ng/build.sh
@@ -0,0 +1,7 @@
+./configure --prefix=/usr                           \
+            --docdir=/usr/share/doc/procps-ng-4.0.5 \
+            --disable-static                        \
+            --disable-kill                          \
+            --enable-watch8bit
+make
+make DESTDIR=$DESTDIR install
