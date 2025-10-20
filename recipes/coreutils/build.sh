281358a1101258da711268797cf94e18e7b4ee8c
Priyanshu Dangare <aspizu@protonmail.com>
2025-10-20 17:26:45 +0000

    fix pkg coreutils

--- recipes/coreutils/build.sh
+++ recipes/coreutils/build.sh
@@ -1,3 +1,5 @@
+autoreconf -fv
+automake -af
 FORCE_UNSAFE_CONFIGURE=1 ./configure --prefix=/usr --libexecdir=/usr/lib --with-openssl --enable-no-install-program=kill,uptime
 make
 make DESTDIR=$DESTDIR install
