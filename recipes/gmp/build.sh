2ccdd64eac9408473620b77b09d236f90dd40046
Priyanshu Dangare <aspizu@protonmail.com>
2025-10-20 16:12:37 +0000

    make gmp not optimize to host processor specifics

--- recipes/gmp/build.sh
+++ recipes/gmp/build.sh
@@ -1,6 +1,7 @@
 sed -i '/long long t1;/,+1s/()/(...)/' configure
 ./configure --prefix=/usr    \
             --enable-cxx     \
-            --disable-static
+            --disable-static \
+            --host=none-linux-gnu
 make
 make install
