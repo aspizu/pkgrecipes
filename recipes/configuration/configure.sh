7e2fb733e1440949bbe45bf8dc7f2a0cb1877d69
Priyanshu Dangare <aspizu@protonmail.com>
2025-10-24 13:56:54 +0530

    fix conf

--- recipes/configuration/configure.sh
+++ recipes/configuration/configure.sh
@@ -1,10 +1,21 @@
 reconfigure() {
-    if [[ -f /var/lib/meow/installed/configuration/reconfigure.done ]]; then
-        echo "No need to reconfigure."
-        exit
-    fi
-    touch /var/lib/meow/installed/configuration/reconfigure.done
-    pwconv
-    grpconv
-    make-ca -g
+
+if [[ -f /var/lib/meow/installed/configuration/reconfigure.done ]]; then
+    echo "No need to reconfigure."
+    exit
+fi
+touch /var/log/{btmp,lastlog,faillog,wtmp}
+chgrp -v utmp /var/log/lastlog
+chmod -v 664  /var/log/lastlog
+chmod -v 600  /var/log/btmp
+touch /var/lib/meow/installed/configuration/reconfigure.done
+ln -sv /proc/self/mounts /etc/mtab
+cat > /etc/hosts << EOF
+127.0.0.1  localhost $(hostname)
+::1        localhost
+EOF
+pwconv
+grpconv
+make-ca -g
+
 }
