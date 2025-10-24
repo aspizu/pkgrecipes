fb8c71337353d4303bbf5807430d7f46b3b0f17a
Priyanshu Dangare <aspizu@protonmail.com>
2025-10-24 10:28:31 +0530

    fix pkg coreutils

--- recipes/coreutils/build.sh
+++ recipes/coreutils/build.sh
@@ -3,5 +3,5 @@ automake -af
 FORCE_UNSAFE_CONFIGURE=1 ./configure --prefix=/usr --libexecdir=/usr/lib --with-openssl --enable-no-install-program=kill,uptime
 make
 make DESTDIR=$DESTDIR install
-mkdir -p /usr/sbin /usr/share/man/man8
+mkdir -p $DESTDIR/usr/sbin $DESTDIR/usr/share/man/man8
 mv $DESTDIR/usr/bin/chroot $DESTDIR/usr/sbin/chroot
