9fc4ab41b96a736e01071c753a05e735ebc1fe1d
Priyanshu Dangare <aspizu@protonmail.com>
2025-10-24 10:25:37 +0530

    fix coreutils pkg

--- recipes/coreutils/build.sh
+++ recipes/coreutils/build.sh
@@ -4,4 +4,4 @@ FORCE_UNSAFE_CONFIGURE=1 ./configure --prefix=/usr --libexecdir=/usr/lib --with-
 make
 make DESTDIR=$DESTDIR install
 mkdir -p /usr/sbin /usr/share/man/man8
-mv $DESTDIR/usr/bin/chroot $DESTDIR/usr/sbin
+mv $DESTDIR/usr/bin/chroot $DESTDIR/usr/sbin/chroot
