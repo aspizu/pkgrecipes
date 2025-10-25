dc7e49e36bcad819a563ffd88a6345fea9f068a5
Priyanshu Dangare <aspizu@protonmail.com>
2025-10-25 12:01:25 +0530

    add pkg cmake

---
+++ recipes/cmake/build.sh
@@ -0,0 +1,10 @@
+sed -i '/"lib64"/s/64//' Modules/GNUInstallDirs.cmake
+
+./bootstrap --prefix=/usr        \
+            --system-libs        \
+            --mandir=/share/man  \
+            --no-system-jsoncpp  \
+            --no-system-cppdap   \
+            --no-system-librhash
+make
+make DESTDIR=$DESTDIR install
