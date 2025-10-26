d4a86e2788ece201ca5b597e6a61dac212d8fbe7
Priyanshu Dangare <aspizu@protonmail.com>
2025-10-26 09:39:44 +0530

    fix git pkg to use symlinks

--- recipes/git/build.sh
+++ recipes/git/build.sh
@@ -2,4 +2,4 @@
             --with-gitconfig=/etc/gitconfig \
             --with-python=python3
 make
-make DESTDIR=$DESTDIR perllibdir=/usr/lib/perl5/5.42/site_perl install
+make DESTDIR=$DESTDIR NO_INSTALL_HARDLINKS=1 perllibdir=/usr/lib/perl5/5.42/site_perl install
